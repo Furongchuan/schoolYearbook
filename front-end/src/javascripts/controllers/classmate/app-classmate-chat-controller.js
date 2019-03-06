350c550b0bd8002626109ced5340e83990b3a47f
dmFu <furongchuan@outlook.com>
2019-03-06 16:26:04 +0800

    聊天室试图渲染完成

--- front-end/src/javascripts/controllers/classmate/app-classmate-chat-controller.js
+++ front-end/src/javascripts/controllers/classmate/app-classmate-chat-controller.js
@@ -1,15 +1,13 @@
 
 import appClassmateChat from '@views/routes/app-classmate-chat.html'
+import appClassmateChatInfo from'@views/routes/app-classmate-chat-info.html'
+import { connectionWebsocket } from '@models/classmate-model'
 
-import { postChatMessage,connectionWebsocket } from '@models/classmate-model'
-
-const render = (req, res, next) => {
-    res.render(appClassmateChat)
-    websocketEstablishment();
+const render = (req, res, next) => { 
+    res.render(appClassmateChat)  
+    websocketEstablishment(req, res, next);
 }
-
-
-function websocketEstablishment () {
+function websocketEstablishment (req, res, next) {
     let socket = connectionWebsocket(); // websocket连接
     socket.addEventListener('open', function (event) {
         $('#send').click('click', (e) => {
@@ -26,12 +24,24 @@ function websocketEstablishment () {
         console.log('socket is close')
     });
     socket.addEventListener('message', function (event) {
-        console.log('Message from server', JSON.parse(event.data));
+        let arr = [];
+        let data = JSON.parse(event.data)
+        if(Array.isArray(data)) {
+            arr = data;
+        }else if(typeof data === 'object'){
+            arr.push(data)
+        }
+        $('.direct-chat-messages').append(
+            template.compile(appClassmateChatInfo)({
+                items: arr
+            })
+        ) 
     })
+    
 }
 // 发送信息
 function sendMessage(socket) {
-    let name = 'abc';
+    let name = '大广';
     let message = $('#message').val();
     $('#message').val('');
     socket.send(JSON.stringify({
